3bc6724e96a07f7bedae04cafd22cef7ee6cf4c1
ZayCodes191 <203040090@mail.unpas.ac.id>
2022-10-05 16:25:00 +0700

    Tugas Prakweb Aplikasi CRUD & OOP

---
+++ oop/Video ke-2/coba.php
@@ -0,0 +1,20 @@
+<!-- 
+Muhammad A. Zaska 
+203040090 
+-->
+
+<?php
+
+class Coba {
+
+
+}
+
+//membuat object instance dari class 1
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ oop/Video ke-2/produk.php
@@ -0,0 +1,16 @@
+<!-- 
+Muhammad A. Zaska 
+203040090 
+-->
+<?php
+
+//jualan produk
+// Komik
+// Game
+
+class Produk {
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
