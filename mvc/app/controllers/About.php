829494b24b09c2d86f88c084ec33a796f9f6e4a1
ZayCodes191 <203040090@mail.unpas.ac.id>
2022-10-11 10:42:00 +0700

    MVC View

--- mvc/app/controllers/About.php
+++ mvc/app/controllers/About.php
@@ -1,12 +1,22 @@
 <?php
-class About
+class About extends Controller
 {
-  public function index($nama = 'Muhamad A. Zay', $pekerjaan = 'Mahasiswa')
+  public function index($nama = 'Muhamad A. Zay', $pekerjaan = 'Mahasiswa', $umur = 21)
   {
-    echo "Halo, nama saya $nama, saya seorang $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About Me';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
+    
   }
   public function page()
   {
-    echo 'About/page';
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
