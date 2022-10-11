b380563d9dbb66436a30c10b87817ec74a5e453f
ZayCodes191 <203040090@mail.unpas.ac.id>
2022-10-11 10:48:14 +0700

    MVC Assets

---
+++ mvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_2022_b_203040090/mvc/public');
--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,3 +1,6 @@
-
+<div class="container">
     <h1> halaman about me </h1>
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+    <img src="<?= BASEURL; ?>/img/profil.jpg" alt="Muhammad A. Zaska" width="200" class="rounded-circle shadow">
+
+</div>
