dda9d7cbdc5ff5321977ca57ee5f456626152348
ZayCodes191 <203040090@mail.unpas.ac.id>
2022-10-12 17:56:20 +0700

    MVC Model

---
+++ mvc/app/models/User_model.php
@@ -0,0 +1,8 @@
+<?php 
+class User_model {
+  private $nama = 'Zay';
+
+  public function getuser(){
+    return $this->nama;
+  }
+}
--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <h1> halaman about me </h1>
+    <h1> This is About Me </h1>
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
     <img src="<?= BASEURL; ?>/img/profil.jpg" alt="Muhammad A. Zaska" width="200" class="rounded-circle shadow">
 
